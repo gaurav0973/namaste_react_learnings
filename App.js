1e7cd8e4b57da588fd1dd2db87b2b98138ce700d
gaurav0973 <codejourneypanda@gmail.com>
2025-02-18 15:12:16 +0530

    this is the Eposode-1 : Inception

---
+++ App.js
@@ -0,0 +1,41 @@
+// const heading = React.createElement('h1', {}, "Kya haal hai bhai..")
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(heading)
+
+
+// creating a nested element in react 
+/*
+<div id="parent">
+    <div id="child">
+        <h1>Mai baccha hu</h1>
+    </div>
+</div>
+🤔💭 andar se bahar ki taraf socho -> like a budding artist
+*/
+
+// const heading = React.createElement('h1', {}, "Mai baccha hu...")
+// const child = React.createElement('div', {id: 'child'}, heading)
+// const parent = React.createElement('div', {id: 'parent'}, child)
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(parent)
+
+
+// creating a sibling, we have to pass them in an array 
+/*
+<h1 id="child1">child1</h1>
+<h1 id="child2">child2</h1>
+*/
+
+const c1 = React.createElement('h1', {id: 'child1'}, "child 1")
+const c2 = React.createElement('h1', {id: 'child2'}, "child 2")
+const parent = React.createElement('div', {}, [c1, c2])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent)
+
+
+
+
+
+
+
---
+++ Readme.md
@@ -0,0 +1,1 @@
+.. Readme bhi banega , sabra karo 
